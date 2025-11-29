d9fa42ea279577e6074c4ebbce40a38c04841281
José Isaac <joseisaacnascimento@gmail.com>
2025-11-29 11:38:59 -0300

    feat(home): importar função getStatus e pageBlock

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -1,17 +1,23 @@
+import getStatusProject from "@/lib/getStatusProject";
 import Header from "@/components/header/Header";
 import Hero from "@/components/sections/HeroSection";
 import Sobre from "@/components/sections/AboutSection";
 import Produtos from "@/components/sections/ProductsSection";
 import Footer from "@/components/Footer";
+import PageBlock from "@/components/PageBlock";
 
-export default function Home() {
+export default async function Home() {
+  // busca o status do projeto no banco
+  const { ativo, mensagemBloqueio } = await getStatusProject();
   return (
-    <main>
+    // se o projeto estiver bloqueado, bloqueia o scroll
+    <main className={!ativo ? "overflow-y-hidden h-screen" : ""}>
       <Header />
       <Hero />
       <Sobre />
       <Produtos />
       <Footer />
+      {!ativo && <PageBlock mensagem={mensagemBloqueio} />}
     </main>
   );
 }
